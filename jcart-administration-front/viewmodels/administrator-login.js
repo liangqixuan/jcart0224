72bcbf04dd8104a89b964d5e78a245dd007b79e1
qixuan.liang <liangqixuan0121@foxmail.com>
2020-03-05 14:55:06 +0800

    update administrator web

--- jcart-administration-front/viewmodels/administrator-login.js
+++ jcart-administration-front/viewmodels/administrator-login.js
@@ -4,28 +4,28 @@ var app = new Vue({
         username: '',
         password: ''
     },
-    methods:{
-        handleLoginClick() {
+    methods: {
+        handleLoginClick(){
             console.log('login click');
             this.loginAdministrator();
         },
         loginAdministrator() {
             axios.get('/administrator/login', {
                 params: {
-                  username: this.username,
-                  password: this.password
+                    username: this.username,
+                    password: this.password
                 }
-              })
-              .then(function (response) {
-                console.log(response);
-                var dto = response.data;
-                localStorage['jcartToken'] = dto.token;
-                localStorage['expireTimestamp'] = dto.expireTimestamp;
-                alert('登录成功')
-              })
-              .catch(function (error) {
-                console.log(error);
-              })
+            })
+                .then(function (response) {
+                    console.log(response);
+                    var dto = response.data;
+                    localStorage['jcartToken'] = dto.token;
+                    localStorage['expireTimestamp'] = dto.expireTimestamp;
+                    alert('登陆成功');
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
     }
 })
