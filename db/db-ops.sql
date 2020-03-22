965bfbd1fbcbd78d7396daf1deac64333c9fcbd5
qixuan.liang <liangqixuan0121@foxmail.com>
2020-03-22 11:19:59 +0800

    droft operation 创建表文档

--- db/db-ops.sql
+++ db/db-ops.sql
@@ -1,7 +1,13 @@
+SET
+  FOREIGN_KEY_CHECKS = 0
+#-----------------------------------------------------------
  DROP TABLE IF EXISTS `jcart_product_detail`;
 CREATE TABLE `jcart_product_detail` (
     `product_id` int NOT NULL,
-    `description` text not null,
-    `other_pic_urls` varchar(2000),
+    `all_count` int not null,
+    `day_count` int not null,
+    `recent_time` datetime not null,
     PRIMARY KEY (`product_id`)
+    index `all_count` (`all_count`),
+    index `day_count` (`day_count`)
   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
